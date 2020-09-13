79aed81346dbc1002c9760ee56035c2863940acf
Maxime Doutreluingne <53557695+mdoutreluingne@users.noreply.github.com>
2020-09-13 15:11:39 +0200

    Mise en place du login github

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class LoginController extends AbstractController
+{
+    private $githubId;
+
+    public function __construct($githubId)
+    {
+        $this->githubId = $githubId;
+    }
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function index()
+    {
+        return $this->render('login/index.html.twig', [
+            'controller_name' => 'LoginController',
+        ]);
+    }
+
+    /**
+     * @Route("/login/github", name="github")
+     */
+    public function github(UrlGeneratorInterface $generator)
+    {
+        $url = $generator->generate("dashboard", [], UrlGeneratorInterface::ABSOLUTE_URL);
+        return new RedirectResponse("https://github.com/login/oauth/authorize?client_id=$this->githubId&redirect_uri=".$url);
+    }
+
+    /**
+     * @Route("/dashboard", name="dashboard")
+     */
+    public function dashboard()
+    {
+        new Response("<body><h1>Hello</h1></body>");
+    }
+}
